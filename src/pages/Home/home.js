b629d44d5afefb8a544f9b21325bcb37016d8315
Emilly <emillymarques96622@gmail.com>
2022-08-10 15:30:08 -0300

    resolvendo bugs

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -1,23 +1,29 @@
 import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 import { client } from '../../service/client'
 import './style.css'
 export const Home = () => {
   const [movie, setMovie] = useState()
   useEffect(() => {
     client.get('/movies').then((response) => {
-      setMovie(response.data.map(item => item.title))
+      setMovie(response.data)
     })
   }, [])
 
   return (
     <div>
       <section className="listMovies">
-        <ul>
-          <li>
-            {movie}
-          </li>
-        </ul>
+<ul>
+{movie.map((movie, index) => (
+  <li key={index}>
+    <Link to={`/movies/${movie.id}`}>{movie.title}</Link>
+  </li>
+)
+
+)}
+</ul>
       </section>
+
     </div>
   )
 }
