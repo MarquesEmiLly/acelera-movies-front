15406a6143fd7e70657780bf76f0763f34a0a546
Emilly <emillymarques96622@gmail.com>
2022-08-15 15:56:02 -0300

    dando função aos inputs

--- src/components/modal/modal.js
+++ src/components/modal/modal.js
@@ -1,10 +1,31 @@
 import { useState } from 'react'
 import './style.css'
 export const Modal = () => {
+  let title = ''
+  let subtitle = ''
+  let resume = ''
+  let data = ''
   const [toggle, setToggle] = useState(false)
   const handlerClick = () => {
     setToggle(!toggle)
   }
+  const handleChangeTitle = (event) => {
+    title = event.target.value
+  }
+  const handleChangeSubtitle = (event) => {
+    subtitle = event.target.value
+  }
+  const handleChangeResume = (event) => {
+    resume = event.target.value
+  }
+  const handleChangeData = (event) => {
+    data = event.target.value
+  }
+  const submit = () => {
+    alert(`SALVANDO O NOVO FILME: ${title}`)
+    console.log(subtitle, resume, data)
+  }
+
   return (
     <div className="tela">
       <nav>
@@ -18,27 +39,28 @@ export const Modal = () => {
               <button onClick={handlerClick} className="close">x</button>
             </header>
             <form className='form1'>
-              <input type="text" placeholder='Title' />
-              <input type="text" placeholder='subtitle' />
+              <input type="text" placeholder='Title' onChange={handleChangeTitle} />
+              <input type="text" placeholder='subtitle' onChange={handleChangeSubtitle} />
             </form>
             <br></br>
-              <input type="text" placeholder='Resume' className='resume'/>
-              <br></br>
-              <form className='form2'>
-              <input type="text" placeholder='data'/>
-              <input type="text" placeholder='imagem'/>
-              </form>
-              <br></br>
-              <form className='form2'>
-              <input type="text" placeholder='diretor'/>
-              <input type="text" placeholder='escritor'/>
-              </form>
-              <br></br>
-              <form className='form2'>
-              <input type="text" placeholder='classificação'/>
-              <input type="text" placeholder='studio'/>
-              </form>
-              <input type="text" placeholder='stars' className='resume'/>
+            <input type="text" placeholder='Resume' className='resume' onChange={handleChangeResume}/>
+            <br></br>
+            <form className='form2'>
+              <input type="text" placeholder='data' onChange={handleChangeData } />
+              <input type="text" placeholder='imagem' />
+            </form>
+            <br></br>
+            <form className='form2'>
+              <input type="text" placeholder='diretor' />
+              <input type="text" placeholder='escritor' />
+            </form>
+            <br></br>
+            <form className='form2'>
+              <input type="text" placeholder='classificação' />
+              <input type="text" placeholder='studio' />
+            </form>
+            <input type="text" placeholder='stars' className='resume' />
+            <button onClick={submit} className='enviar'>enviar</button>
           </section>
         </div>)
       }
