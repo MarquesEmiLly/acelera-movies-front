0dc3a9b15e331666712147a0c9891046bb423777
Emilly <emillymarques96622@gmail.com>
2022-08-18 20:46:53 -0300

    movies por id

--- src/pages/Movies/movies.js
+++ src/pages/Movies/movies.js
@@ -1,18 +1,26 @@
+// import { client } from '../../service/client'
+
 import { useEffect, useState } from 'react'
 import { client } from '../../service/client'
+import './style.css'
 
 export const Movies = () => {
-  const [movies, setMovie] = useState([])
+  const [id, setMovie] = useState([])
   useEffect(() => {
-    client.get('/movies').then((response) => {
+    client.get('/movies/:11').then((response) => {
       setMovie(response.data)
     })
   }, [])
 
   return (
-    <>
-      <h1>Pagina movies</h1>
-      <h2> {movies} </h2>
-    </>
+    <div className='container'>
+      <h1>Filmes por id:</h1>
+        {id.map((movie, id) => (
+         <h2 key = { id } >
+          <p to={`/movies/:11 ${movie.id}`}>{movie.title}</p>
+          </h2>
+        )
+        )}
+        </div>
   )
 }
